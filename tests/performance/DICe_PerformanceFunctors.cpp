f3ea863c942b516e13b87c9f0ee233a5f9fee740
Dan Turner <dzturne@sandia.gov>
2015-09-14 12:18:27 -0600

    Added timers to performance tests

--- tests/performance/DICe_PerformanceFunctors.cpp
+++ tests/performance/DICe_PerformanceFunctors.cpp
@@ -42,6 +42,7 @@
 
 #include <DICe.h>
 #include <DICe_Image.h>
+#include <DICe_Subset.h>
 #include <DICe_Kokkos.h>
 #include <DICe_Tiff.h>
 
@@ -74,6 +75,7 @@ int main(int argc, char *argv[]) {
 
   // create a vector of image sizes to use
   const size_t num_img_sizes = 5;
+  const size_t num_time_samples = 5;
   size_t width = 0;
   size_t height = 0;
   std::vector<size_t> widths(num_img_sizes,0);
@@ -91,40 +93,171 @@ int main(int argc, char *argv[]) {
     heights[i] = (i+1)*h_step;
   }
 
-  // set up the parameters
   Teuchos::RCP<Teuchos::ParameterList> params = rcp(new Teuchos::ParameterList());
-  params->set(DICe::compute_image_gradients,true);
+  params->set(DICe::gauss_filter_mask_size,13);
+  Teuchos::RCP<Image> img;
+  Teuchos::RCP<Def_Map> map = Teuchos::rcp(new Def_Map());
+  map->u_ = 1.25; // TODO randomize deformation map
+  map->v_ = -1.456; // needs to remain inside the current image
+  map->t_ = 0.001;
+  map->ex_ = 0.002;
+  map->ey_ = 0.001;
+  map->g_ = 0.0;
+  Teuchos::RCP<Subset> subset;
+  const size_t subset_edge_buffer = 10; // must be larger than the deformations above
 
-  // image size loop:
-  for(size_t size_it=0;size_it<num_img_sizes;++size_it){
-    const size_t w_it = widths[size_it];
-    const size_t h_it = heights[size_it];
-    *outStream << "image size: " << w_it << " x " << h_it << std::endl;
+  std::vector<scalar_t> sizes(num_img_sizes,0.0);
+  std::vector<scalar_t> read_times(num_img_sizes,0.0);
+  std::vector<scalar_t> grad_times(num_img_sizes,0.0);
+  std::vector<scalar_t> filter_times(num_img_sizes,0.0);
+  std::vector<scalar_t> sub_construct_times(num_img_sizes,0.0);
+  std::vector<scalar_t> sub_init_times(num_img_sizes,0.0);
+  std::vector<scalar_t> sub_bilinear_times(num_img_sizes,0.0);
+  std::vector<scalar_t> sub_keys_times(num_img_sizes,0.0);
+  std::vector<scalar_t> mean_times(num_img_sizes,0.0);
+  std::vector<scalar_t> corr_times(num_img_sizes,0.0);
 
-    *outStream << "reading the image" << std::endl;
-    {
+
+  // num timing samples loop
+  for(size_t time_sample=0;time_sample<num_time_samples;++time_sample){
+    *outStream << "%%% time sample " << time_sample << std::endl;
+    // image size loop:
+    for(size_t size_it=0;size_it<num_img_sizes;++size_it){
+      const size_t w_it = widths[size_it];
+      const size_t h_it = heights[size_it];
+      *outStream << "---------------------------------------------------------------------------" << std::endl;
+      *outStream << "image size: " << w_it << " x " << h_it << std::endl;
+      if(time_sample==0) sizes[size_it] = w_it*h_it;
+
+      // read image
+      *outStream << "reading the image" << std::endl;
       cpu_timer read_timer;
-      Image img(file_name,0,0,w_it,h_it,params);
-      *outStream << read_timer.format() << std::endl;
-    }
+      {
+        read_timer.start();
+        img = Teuchos::rcp(new Image(file_name,0,0,w_it,h_it));
+        read_timer.stop();
+      }
+      *outStream << "** read time" << read_timer.format();
+      read_times[size_it] += ((scalar_t)(read_timer.elapsed().wall)/1000000000)/num_time_samples;
 
-    // TODO image gradient
+      // gradient
+      *outStream << "computing the image gradient" << std::endl;
+      cpu_timer grad_timer;
+      {
+        grad_timer.start();
+        img->compute_gradients();
+        grad_timer.stop();
+      }
+      *outStream << "** grad time" << grad_timer.format();
+      grad_times[size_it] += ((scalar_t)(grad_timer.elapsed().wall)/1000000000)/num_time_samples;
 
+      // image filter
+      *outStream << "computing convolution filter" << std::endl;
+      cpu_timer filter_timer;
+      {
+        filter_timer.start();
+        img->gauss_filter();
+        filter_timer.stop();
+      }
+      *outStream << "** filter time" << filter_timer.format();
+      filter_times[size_it] += ((scalar_t)(filter_timer.elapsed().wall)/1000000000)/num_time_samples;
 
-  } // end size loop
+      // create a subset
+      *outStream << "creating a subset of size " << w_it-subset_edge_buffer << " x " << h_it-subset_edge_buffer << std::endl;
+      cpu_timer subset_construct_timer;
+      {
+        subset_construct_timer.start();
+        subset = Teuchos::rcp(new Subset(w_it/2,h_it/2,w_it-subset_edge_buffer,h_it-subset_edge_buffer));
+        subset_construct_timer.stop();
+      }
+      *outStream <<"** subset constructor time" << subset_construct_timer.format();
+      sub_construct_times[size_it] += ((scalar_t)(subset_construct_timer.elapsed().wall)/1000000000)/num_time_samples;
 
+      // initialize the reference values (copy, no deformation involved)
+      *outStream << "initializing the reference intensities" << std::endl;
+      cpu_timer subset_init_ref_timer;
+      {
+        subset_init_ref_timer.start();
+        subset->initialize(img);
+        subset_init_ref_timer.stop();
+      }
+      *outStream <<"** subset init ref time" << subset_init_ref_timer.format();
+      sub_init_times[size_it] += ((scalar_t)(subset_init_ref_timer.elapsed().wall)/1000000000)/num_time_samples;
 
+      // initialize the deformed values (mapping involved as well as interpolation, cant separate these)
+      *outStream << "initializing the deformed intensities (bilinear)" << std::endl;
+      cpu_timer subset_init_def_bilinear_timer;
+      {
+        subset_init_def_bilinear_timer.start();
+        subset->initialize(img,DEF_INTENSITIES,map,BILINEAR);
+        subset_init_def_bilinear_timer.stop();
+      }
+      *outStream <<"** subset init def bilinear time" << subset_init_def_bilinear_timer.format();
+      sub_bilinear_times[size_it] += ((scalar_t)(subset_init_def_bilinear_timer.elapsed().wall)/1000000000)/num_time_samples;
 
+      *outStream << "re-initializing the deformed intensities (Keys fourth-order)" << std::endl;
+      cpu_timer subset_init_def_keys_timer;
+      {
+        subset_init_def_keys_timer.start();
+        subset->initialize(img,DEF_INTENSITIES,map,KEYS_FOURTH_ORDER);
+        subset_init_def_keys_timer.stop();
+      }
+      *outStream <<"** subset init def Keys time" << subset_init_def_keys_timer.format();
+      sub_keys_times[size_it] += ((scalar_t)(subset_init_def_keys_timer.elapsed().wall)/1000000000)/num_time_samples;
 
+      // mean value
+      *outStream << "computing the mean subset value" << std::endl;
+      cpu_timer subset_mean_timer;
+      {
+        subset_mean_timer.start();
+        const scalar_t mean = subset->mean(REF_INTENSITIES);
+        subset_mean_timer.stop();
+      }
+      *outStream << "** subset mean intensity time" << subset_mean_timer.format();
+      mean_times[size_it] += ((scalar_t)(subset_mean_timer.elapsed().wall)/1000000000)/num_time_samples;
 
+      // correlation
+      *outStream << "computing the correlation between the ref and def intensities" << std::endl;
+      cpu_timer corr_timer;
+      {
+        corr_timer.start();
+        const scalar_t gamma = subset->gamma();
+        corr_timer.stop();
+      }
+      *outStream << "** correlation time" << corr_timer.format();
+      corr_times[size_it] += ((scalar_t)(corr_timer.elapsed().wall)/1000000000)/num_time_samples;
 
-  // TODO image filter/convolution
+    } // end size loop
 
-  // TODO subset mean value
+  } // end time sample loop
 
-  // TODO subset initialization(mapping + interpolation)
+  *outStream << "\n\nTiming summary: " << std::endl;
+  *outStream << std::setw(15) << "size" <<
+      std::setw(15) << "read"<<
+      std::setw(15) << "grad" <<
+      std::setw(15) << "filter" <<
+      std::setw(15) << "subset" <<
+      std::setw(15) << "init ref" <<
+      std::setw(15) << "bilinear" <<
+      std::setw(15) << "keys" <<
+      std::setw(15) << "mean" <<
+      std::setw(15) << "correlation" <<
+      std::endl;
+  for(size_t i=0;i<num_img_sizes;++i){
+    *outStream << std::setw(15) <<
+        sizes[i] << std::setw(15) <<
+        read_times[i] << std::setw(15) <<
+        grad_times[i] << std::setw(15) <<
+        filter_times[i] << std::setw(15) <<
+        sub_construct_times[i] << std::setw(15) <<
+        sub_init_times[i] << std::setw(15) <<
+        sub_bilinear_times[i] << std::setw(15) <<
+        sub_keys_times[i] << std::setw(15) <<
+        mean_times[i] << std::setw(15) <<
+        corr_times[i] << std::setw(15) <<
+        std::endl;
+  }
 
-  // TODO compute correlation value
 
   *outStream << "--- End performance test ---" << std::endl;
 
