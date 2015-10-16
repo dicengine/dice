0b74e5fb4a115e7fbee6e9ae0564403a6f48d714
Dan Turner <dzturne@sandia.gov>
2015-10-16 05:58:48 -0600

    Added test for DIC challenge 14 with Keys4 strain post processor

---
+++ tests/regression/dic_challenge_14_keys4/plot_compare.py
@@ -0,0 +1,25 @@
+# Import everything from matplotlib (numpy is accessible via 'np' alias)
+from pylab import *
+import matplotlib.pyplot as plt
+
+font = {'family' : 'sans-serif',
+    'weight' : 'regular',
+    'size'   : 8}
+matplotlib.rc('font', **font)
+
+DATA = loadtxt('avg_results_strain_L1.txt')
+X        = DATA[:,0]
+DISP     = DATA[:,1]
+COMMAND  = DATA[:,2]
+DIFF     = DATA[:,3]
+
+fig = figure(figsize=(12,5), dpi=150)
+plot(X,DISP,'-b')
+plot(X,COMMAND,'-k')
+fig.set_tight_layout(True)
+xlabel('x position(px)')
+ylabel('x displacement(px)')
+xlim(0.0,2048.0)
+legend(["Computed","Command"])
+
+show()   
