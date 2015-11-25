dc6007774d388e304193c70674b607dcdb94016c
Dan Turner <dzturne@sandia.gov>
2015-11-25 14:15:24 -0700

    Added another example: obstructions

---
+++ tests/examples/obstruction/plot_data.py
@@ -0,0 +1,38 @@
+# Import everything from matplotlib (numpy is accessible via 'np' alias)
+from pylab import *
+import matplotlib.pyplot as plt
+
+NUM_SUBSETS = 2
+FILE_PREFIX = "./results/DICe_solution_"
+
+font = {'family' : 'sans-serif',
+        'weight' : 'regular',
+        'size'   : 8}
+matplotlib.rc('font', **font)
+
+for i in range(0,NUM_SUBSETS):
+  FILE = FILE_PREFIX+str(i)+".txt"
+  PDFTHETA = "Results_"+str(i)+".pdf"
+  print(FILE)
+  DATA = loadtxt(FILE,delimiter=',',skiprows=21)
+  FRAME    = DATA[:,0]
+  X_TILDE  = DATA[:,1]
+  Y_TILDE  = DATA[:,2]
+  DISP_X   = DATA[:,3]
+  DISP_Y   = DATA[:,4]
+  THETA    = DATA[:,5]
+  SIGMA    = DATA[:,6]
+  GAMMA    = DATA[:,7]
+  fig = figure(figsize=(12,5), dpi=150)
+  plot(FRAME,DISP_X,'-b')
+  plot(FRAME,DISP_Y,'-r')
+  plot(FRAME,THETA,'-g')
+  fig.set_tight_layout(True)
+  legend(["Displacement X","Displacement Y","Rotation"])
+  xlabel('Image Number')
+  ylabel('Displacement (pixels) or Rotation (Rad)')
+  title("Subset ID: "+str(i))
+  savefig(PDFTHETA,dpi=150, format='pdf')
+  
+show()    
+
