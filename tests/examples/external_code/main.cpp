efa2e1883ffbda583dca7dd644ee9b4e536672aa
Dan Turner <dzturne@sandia.gov>
2015-11-27 13:01:46 -0700

    Major changes to cmake build system (lib and bin output dirs)
    Set all library, runtime, and archive directories to be in a master
    lib or bin dir the in the build folder. This puts all the libs and
    executables in one place, even if the 'install' target is not made.
    
    Started an external code example that links against DICe libraries
    by using an externalproject_add() in cmake. This was part of the
    motivation for putting all the libs and execs in one place.

---
+++ tests/examples/external_code/main.cpp
@@ -0,0 +1,13 @@
+//
+// An example used to illustrate calling DICe routines from an external code
+//
+#include <iostream>
+int main(int argc, char *argv[]) {
+
+  std::cout << "Hello world!\n";
+
+  std::cout << "End Result: TEST PASSED\n";
+  
+  return 0;
+}
+
