407d77727768c09475136bc4558f55b2037fa9d5
Dan Turner <dzturne@sandia.gov>
2016-01-18 11:08:58 -0700

    Added nightly regression test scripts to repo

---
+++ nightly/LocalDefinitions_Sample_.py
@@ -0,0 +1,7 @@
+MACHINE_NAME = 'ceerws3705.srn.sandia.gov'
+OPERATING_SYSTEM = '(Linux x86_64)'
+LOG_FILE_DIRECTORY = '/scratch/code/nightly_testing/logs'
+TRILINOS_ROOT = '/scratch/code/trilinos_nightly/'
+TRILINOS_ACTIVE_CONFIGS = ["build_serial_debug","build_serial_release","build_mpi_debug","build_mpi_release","build_manycore_debug","build_manycore_release"]
+DICE_ROOT = '/scratch/code/DICe_nightly/'
+DICE_ACTIVE_CONFIGS = ["build_serial_debug","build_serial_release","build_mpi_debug","build_mpi_release","build_manycore_debug","build_manycore_release","build_double_debug"]
---
+++ nightly/UpdateTrilinos.py
@@ -0,0 +1,97 @@
+#! /usr/bin/env python
+
+import string
+from subprocess import Popen
+import os
+import shutil
+from Utils import now, append_time, force_write
+from LocalDefinitions import TRILINOS_ROOT
+
+def update_trilinos(logfile, build_type):
+
+    trilinos_build_dir = TRILINOS_ROOT + "/" + build_type
+    message = ""
+    status = "Passed"
+    
+    logfile.write(append_time("Updating Trilinos with build type "+build_type+" ")+"\n") ; force_write(logfile)
+
+    # ---- Clean Trilinos ----
+
+    logfile.write(append_time("\nCleaning Trilinos")+"\n") ; force_write(logfile)
+    if os.path.exists(trilinos_build_dir+'/include'):
+        shutil.rmtree(trilinos_build_dir+'/include')
+    if os.path.exists(trilinos_build_dir+'/lib'):
+        shutil.rmtree(trilinos_build_dir+'/lib')
+    os.chdir(trilinos_build_dir)
+    command = ["make", "clean"]
+    p = Popen(command, stdout=logfile, stderr=logfile)
+    return_code = p.wait()
+    force_write(logfile)
+    msg = "Trilinos " + build_type + " clean:  Passed\n"
+    if return_code != 0:
+        msg = "Trilinos " + build_type + " clean:  FAILED\n"
+        status = "FAILED"
+    logfile.write(msg) ; force_write(logfile)
+    message += msg
+    logfile.write(append_time("\nTrilinos clean complete")+"\n") ; force_write(logfile)
+    
+    # ---- update Trilinos ----
+
+    logfile.write(append_time("\nUpdating Trilinos")+"\n") ; force_write(logfile)
+    os.chdir(TRILINOS_ROOT)
+    command = ["eg", "pull"]
+    p = Popen(command, stdout=logfile, stderr=logfile)
+    return_code = p.wait()
+    force_write(logfile)
+    msg = "Trilinos " + build_type + " eg pull:  Passed\n"
+    if return_code != 0:
+       msg = "Trilinos " + build_type + " eg pull:  FAILED\n"
+       status = "FAILED"
+    logfile.write(msg) ; force_write(logfile)
+    message += msg
+    logfile.write(append_time("\nTrilinos update complete")+"\n") ; force_write(logfile)
+
+    # ---- Run CMake for Trilinos ----
+
+    logfile.write(append_time("\nRunning CMake for Trilinos")+"\n") ; force_write(logfile)
+    os.chdir(trilinos_build_dir)
+    command = ["./do-cmake"]
+    p = Popen(command, stdout=logfile, stderr=logfile, shell=True, executable='/bin/bash')
+    return_code = p.wait()
+    force_write(logfile)
+    msg = "Trilinos " + build_type + " CMake:  Passed\n"
+    if return_code != 0:
+        msg = "Trilinos " + build_type + " CMake:  FAILED\n"
+        status = "FAILED"
+    logfile.write(msg) ; force_write(logfile)
+    message += msg
+    logfile.write(append_time("\nCMake for Trilinos complete")+"\n") ; force_write(logfile)
+
+    # ---- Build and install Trilinos ----
+
+    logfile.write(append_time("\nBuilding Trilinos")+"\n") ; force_write(logfile)
+    os.chdir(trilinos_build_dir)
+    command = ["./do-make"]
+    p = Popen(command, stdout=logfile, stderr=logfile)
+    return_code = p.wait()
+    force_write(logfile)
+    msg = "Trilinos " + build_type + " build:  Passed\n"
+    if return_code != 0:
+        msg = "Trilinos " + build_type + " build:  FAILED\n"
+        status = "FAILED"
+    logfile.write(msg) ; force_write(logfile)
+    message += msg
+    logfile.write(append_time("\nTrilinos build complete")+"\n") ; force_write(logfile)
+
+    return (status, message)
+
+if __name__ == "__main__":
+
+    print "\n--UpdateTrilinos.py--\n"
+    log_file = open("UpdateTrilinos.log", 'w')
+    log_file.write("Trilinos update " + now() + "\n\n") ; force_write(log_file)
+    status, message = update_trilinos(log_file, "Debug")
+    print message
+    status, message = update_trilinos(log_file, "Release")
+    print message
+    print "Log file written to UpdateTrilinos.log\n"
