ed848e123be6e1757743ae7fb9e38c0af25e369e
Dan Turner <dzturne@sandia.gov>
2016-02-29 09:18:22 -0700

    Added test for contrast_level field computed by subset

---
+++ tests/component/DICe_TestConrast.cpp
@@ -0,0 +1,112 @@
+// @HEADER
+// ************************************************************************
+//
+//               Digital Image Correlation Engine (DICe)
+//                 Copyright 2015 Sandia Corporation.
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact: Dan Turner (dzturne@sandia.gov)
+//
+// ************************************************************************
+// @HEADER
+
+#include <DICe.h>
+#include <DICe_Image.h>
+#include <DICe_Subset.h>
+
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_oblackholestream.hpp>
+#include <Teuchos_ParameterList.hpp>
+
+#include <iostream>
+
+#include <random>
+
+using namespace DICe;
+
+int main(int argc, char *argv[]) {
+
+  DICe::initialize(argc, argv);
+
+  // only print output if args are given (for testing the output is quiet)
+  int_t iprint     = argc - 1;
+  int_t errorFlag  = 0;
+  Teuchos::RCP<std::ostream> outStream;
+  Teuchos::oblackholestream bhs; // outputs nothing
+  if (iprint > 0)
+    outStream = Teuchos::rcp(&std::cout, false);
+  else
+    outStream = Teuchos::rcp(&bhs, false);
+
+  *outStream << "--- Begin test ---" << std::endl;
+
+  // create a test image with known std-dev or contrast
+  const int_t width(101);
+  const int_t height(101);
+  scalar_t std_dev = 10.0;
+  scalar_t mean = 128.0;
+  int_t num_levels = 10;
+  for(int_t level=0;level <num_levels;++level){
+    Teuchos::ArrayRCP<intensity_t> intensities(width*height,0.0);
+    std::default_random_engine generator;
+    std::normal_distribution<intensity_t> distribution(mean,std_dev);
+    for(int_t i=0;i<width*height;++i){
+      intensity_t value = distribution(generator);
+      intensities[i] = value;
+    }
+    Teuchos::RCP<DICe::Image> image = Teuchos::rcp(new DICe::Image(width,height,intensities));
+    //image->write("contrast.tif");
+    // create a subset
+    Teuchos::RCP<DICe::Subset> subset = Teuchos::rcp(new DICe::Subset(50,50,50,50));
+    subset->initialize(image,REF_INTENSITIES);
+    subset->initialize(image,DEF_INTENSITIES);
+    scalar_t contrast = subset->contrast_std_dev();
+    *outStream << "input std-dev: " << std_dev << " computed: " << contrast << std::endl;
+    if(std::abs(std_dev - contrast) > 1.0){
+      *outStream << "Error, contrast value is not correct" << std::endl;
+      errorFlag +=1;
+    }
+    std_dev += 10;
+  }
+  *outStream << "--- End test ---" << std::endl;
+
+  DICe::finalize();
+
+  if (errorFlag != 0)
+    std::cout << "End Result: TEST FAILED\n";
+  else
+    std::cout << "End Result: TEST PASSED\n";
+
+  return 0;
+
+}
+
